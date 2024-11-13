ced279c9c95f57d977da61f2efc1f9c9342343c1
bhargav983 <ssy.balu@gmail.com>
2024-11-13 18:53:19 +0530

    added login page to authentication

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.authentication, name='authentication'),
+    path('login/', views.login_view, name='login'),
+    path('home/', views.home_view, name='home'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def authentication(request):
     return render(request, 'authentication.html')
+
+
+def home_view(request):
+    return render(request, 'home.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        print("username =",username)
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  
+        else:
+            messages.error(request, 'Invalid username or password')
+    return render(request, 'login.html')
+
